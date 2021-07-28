cb6ed963febf10a2feee41ccbee8ac9b481e18b6
Shoyayeb <sa.fine@outlook.com>
2021-07-28 16:02:08 +0600

    first section done with responsive and contact modal

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+document.getElementById("cancel").addEventListener("click", closeFunction);
+document.getElementById("contactNav").addEventListener("click", openFunction);
+
+
+function closeFunction() {
+    document.getElementById("contactForm").style.display = "none";
+}
+
+
+function openFunction() {
+    document.getElementById("contactForm").style.display = "block";
+}
